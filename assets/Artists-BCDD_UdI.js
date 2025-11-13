be1f4e121b3ca4115bb2494aa370dfeb8eb8fafb
AmiKonst <tiufyakin@gmail.com>
2025-11-13 14:27:57 +0300

    build

---
+++ assets/Artists-BCDD_UdI.js
@@ -0,0 +1,51 @@
+import { _ as _export_sfc, u as useI18n, s as stores, j as onMounted, g as onBeforeUnmount, c as createElementBlock, k as createBaseVNode, e as createVNode, t as toDisplayString, h as unref, F as Fragment, o as openBlock } from './index-C7KVDunn.js';
+import { _ as _sfc_main$1 } from './Ready-CR-BTIP0.js';
+import { E as EmptyLabel } from './EmptyLabel-B2QA4Mc5.js';
+
+const _hoisted_1 = { class: "head" };
+const _hoisted_2 = { class: "title" };
+
+    
+const _sfc_main = {
+  __name: 'Artists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    // import { storeToRefs } from 'pinia';
+    stores.user();
+    stores.nav();
+    const tg = stores.tg();
+
+    stores.others();
+    // const { ready } = storeToRefs(others);
+
+    onMounted(() => {
+        tg.showBackButton();
+    });
+
+    onBeforeUnmount(() => {
+        tg.hideBackButton();
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    createBaseVNode("div", _hoisted_1, [
+      createBaseVNode("label", _hoisted_2, toDisplayString(unref(t)('pages.favorite-artists.title')), 1)
+    ]),
+    createVNode(EmptyLabel, {
+      class: "empty-label",
+      "animate-icon": "artists",
+      title: unref(t)('pages.favorite-artists.empty.title'),
+      description: unref(t)('pages.favorite-artists.empty.description')
+    }, null, 8, ["title", "description"]),
+    createVNode(_sfc_main$1, { page: "favorite-artists" })
+  ], 64))
+}
+}
+
+};
+const Artists = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-7e149a1b"]]);
+
+export { Artists as default };
+//# sourceMappingURL=Artists-BCDD_UdI.js.map
---
+++ assets/Category-e90MniS5.js
@@ -0,0 +1,52 @@
+import { D as Dictionary } from './Dictionary-DYn_fZro.js';
+import { _ as _sfc_main$1 } from './Ready-CR-BTIP0.js';
+import { a as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, o as openBlock } from './index-C7KVDunn.js';
+import './Img-BRioItgt.js';
+import './EmptyLabel-B2QA4Mc5.js';
+
+const _sfc_main = {
+  __name: 'Category',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'categories');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "category",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Category-e90MniS5.js.map
