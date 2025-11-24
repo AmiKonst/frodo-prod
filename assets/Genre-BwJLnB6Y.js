8cb0155858af312d3717bed3faa9e540b19752f6
AmiKonst <tiufyakin@gmail.com>
2025-11-24 17:59:12 +0300

    Build

---
+++ assets/Genre-BwJLnB6Y.js
@@ -0,0 +1,53 @@
+import { D as Dictionary } from './Dictionary-JTuRYsuP.js';
+import { _ as _sfc_main$1 } from './Ready-BN08cIEQ.js';
+import { a as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, o as openBlock } from './index-C2_PupvE.js';
+import './Img-iGgwcdKe.js';
+import './ItemDetail-1jxiYZ1I.js';
+import './EmptyLabel-B_Z5XnIY.js';
+
+const _sfc_main = {
+  __name: 'Genre',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'genres');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genre-BwJLnB6Y.js.map
---
+++ assets/ItemDetail-1jxiYZ1I.js
@@ -0,0 +1,120 @@
+import { _ as _export_sfc, s as stores, r as reactive, i as ref, k as computed, l as onMounted, g as onBeforeUnmount, c as createElementBlock, o as openBlock, v as createBaseVNode, p as renderSlot, n as normalizeStyle, b as createBlock, d as createCommentVNode, y as normalizeClass, h as unref, I as IconButton } from './index-C2_PupvE.js';
+
+const _hoisted_1 = { class: "detail-item" };
+const _hoisted_2 = { class: "box" };
+
+    
+const _sfc_main = {
+  __name: 'ItemDetail',
+  props: {
+        previewHeight: { type: Number, default: 384 },
+        more: { type: Boolean, default: true }
+    },
+  emits: ['mode', 'more'],
+  setup(__props, { emit: __emit }) {
+
+    const others = stores.others();
+
+    const props = __props;
+
+    const data = reactive({
+        mode: 'preview'
+    });
+
+
+    const emit = __emit;
+
+    const detail = ref(null);
+
+    const scrollTop = ref(0);
+    const scrollDirection = ref(0);
+
+
+    const progress = computed(() =>
+        Math.min(1, scrollTop.value / props.previewHeight)
+    );
+
+    // Opacity
+        const wrapperOpacity = computed(() => 1 - progress.value);
+        const backgroundOpacity = computed(() => 1 - progress.value);
+
+    let lastY = 0;
+    const onScroll = (e) => {
+        const y = e.target.scrollTop;
+        scrollDirection.value = y - lastY;
+        lastY = y;
+
+        scrollTop.value = y;
+        updateMode();
+    };
+
+    const updateMode = () => {
+        const newMode = progress.value >= 1 ? "detail" : "preview";
+        if (newMode !== data.mode) {
+            data.mode = newMode;
+            emit('mode', newMode);
+        }
+    };
+
+
+    document.body.classList.add('with-tg-header');
+    onMounted(() => {
+        document.querySelector('.page').addEventListener('scroll', onScroll);
+    });
+
+    onBeforeUnmount(() => {
+        document.body.classList.remove('with-tg-header');
+        document.querySelector('.page').removeEventListener('scroll', onScroll);
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("div", {
+      class: "background",
+      style: normalizeStyle({
+                opacity: backgroundOpacity.value,
+                height: props.previewHeight + 'px'
+            })
+    }, [
+      renderSlot(_ctx.$slots, "background", {}, undefined, true)
+    ], 4),
+    createBaseVNode("div", _hoisted_2, [
+      createBaseVNode("div", {
+        class: "preview",
+        style: normalizeStyle({
+                    opacity: 1,
+                    height: props.previewHeight + 'px'
+                })
+      }, [
+        createBaseVNode("div", {
+          class: "wrapper",
+          style: normalizeStyle({ opacity: wrapperOpacity.value })
+        }, [
+          (props.more)
+            ? (openBlock(), createBlock(IconButton, {
+                key: 0,
+                icon: "settings",
+                class: normalizeClass(["tertiary size-l more-button", { invert: unref(others).theme !== 'dark' }]),
+                onClick: _cache[0] || (_cache[0] = $event => (emit('more')))
+              }, null, 8, ["class"]))
+            : createCommentVNode("", true),
+          renderSlot(_ctx.$slots, "preview", {}, undefined, true)
+        ], 4)
+      ], 4),
+      createBaseVNode("div", {
+        class: "detail",
+        ref_key: "detail",
+        ref: detail
+      }, [
+        renderSlot(_ctx.$slots, "detail", {}, undefined, true)
+      ], 512)
+    ])
+  ]))
+}
+}
+
+};
+const ItemDetail = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-87152a8c"]]);
+
+export { ItemDetail as I };
+//# sourceMappingURL=ItemDetail-1jxiYZ1I.js.map
