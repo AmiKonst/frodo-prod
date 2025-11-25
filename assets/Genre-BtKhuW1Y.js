82c1c2e598579ff2554c4c438970ef3cb505473b
AmiKonst <tiufyakin@gmail.com>
2025-11-25 13:02:06 +0300

    Build

---
+++ assets/Genre-BtKhuW1Y.js
@@ -0,0 +1,53 @@
+import { D as Dictionary } from './Dictionary-BrmWe9QD.js';
+import { _ as _sfc_main$1 } from './Ready-BtHOr_Xv.js';
+import { a as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, o as openBlock } from './index-D3sHi-jY.js';
+import './Img-DxO743L3.js';
+import './ItemDetail-DuJuk2TQ.js';
+import './EmptyLabel-DLjNoKuZ.js';
+
+const _sfc_main = {
+  __name: 'Genre',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'genres');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genre-BtKhuW1Y.js.map
---
+++ assets/Ready-BtHOr_Xv.js
@@ -0,0 +1,62 @@
+import { s as stores, f as storeToRefs, w as watch, g as onBeforeUnmount } from './index-D3sHi-jY.js';
+
+const _sfc_main = {
+  __name: 'Ready',
+  props: {
+    page: { type: String, default: '' },
+    init: { type: Function, default: async () => {} }
+},
+  emits: ['ready'],
+  setup(__props, { emit: __emit }) {
+
+const nav = stores.nav();
+const others = stores.others();
+
+const { ready } = storeToRefs(others);
+
+const props = __props;
+
+const emit = __emit;
+
+const onReady = async () => {
+    if (props.init) {
+        await props.init();
+    }
+
+    if (props.page === nav.page) {
+        emit('ready');
+    }
+
+    nav.ready(props.page);
+
+    others.setPageReady(true);
+};
+
+others.setPageReady(false);
+
+if (ready.value) {
+    onReady();
+}
+
+watch(
+    () => ready.value,
+    async () => {
+        if (ready.value) {
+            onReady();
+        }
+    }
+);
+
+onBeforeUnmount(() => {
+    others.setPageReady(null);
+});
+
+return (_ctx, _cache) => {
+  return null
+}
+}
+
+};
+
+export { _sfc_main as _ };
+//# sourceMappingURL=Ready-BtHOr_Xv.js.map
