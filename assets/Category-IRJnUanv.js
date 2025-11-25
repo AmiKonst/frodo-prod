69374fc8cd3e0b4508cee157e74975a009bc8fb4
AmiKonst <tiufyakin@gmail.com>
2025-11-25 22:04:40 +0300

    build

---
+++ assets/Category-IRJnUanv.js
@@ -0,0 +1,53 @@
+import { D as Dictionary } from './Dictionary-C4vv33Vy.js';
+import { _ as _sfc_main$1 } from './Ready-DLyYQ5xO.js';
+import { a as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, o as openBlock } from './index-h2KunT5V.js';
+import './Img-Q2Pwm1cO.js';
+import './ItemDetail-Dl9buJW1.js';
+import './EmptyLabel-CtfNmPdt.js';
+
+const _sfc_main = {
+  __name: 'Category',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'categories');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "category",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Category-IRJnUanv.js.map
---
+++ assets/ModalArtistKebab-Bfea0Imt.js
@@ -0,0 +1,106 @@
+import { _ as _export_sfc, u as useI18n, s as stores, r as reactive, l as onMounted, a as api, y as resolveComponent, b as createBlock, o as openBlock, t as withCtx, v as createBaseVNode, e as createVNode, x as toDisplayString, h as unref, d as createCommentVNode, J as BaseModal } from './index-h2KunT5V.js';
+import { I as Img } from './Img-Q2Pwm1cO.js';
+
+const _hoisted_1 = { class: "title" };
+const _hoisted_2 = { class: "kebab-list" };
+
+
+const _sfc_main = {
+  __name: 'ModalArtistKebab',
+  setup(__props) {
+
+const { t } = useI18n();
+
+stores.tg();
+const modals = stores.modals();
+const share = stores.share();
+
+const data = reactive({
+    loading: true
+});
+
+
+const shareIt = () => {
+    share.artist(data.item);
+
+    setTimeout(() => {
+        close();
+    }, 400);
+};
+
+const about = () => {
+    setTimeout(() => {
+        close();
+    }, 400);
+    modals.open('artist', { data: { code: data.item.code } });
+};
+
+const close = () => {
+    modals.close('artist-kebab');
+};
+
+
+onMounted(async () => {
+    const payload = modals.getData('artist-kebab');
+
+    if (!payload?.code) {
+        close();
+        return;
+    }
+
+    data.item = await api.artists().get(payload.code);
+    data.loading = false;
+});
+
+
+return (_ctx, _cache) => {
+  const _component_Icon = resolveComponent("Icon");
+
+  return (openBlock(), createBlock(BaseModal, {
+    name: "artist-kebab",
+    mobileMode: true,
+    closeOutside: true,
+    isLoading: data.loading
+  }, {
+    title: withCtx(() => [
+      createBaseVNode("div", _hoisted_1, [
+        (data.item?.profileImage?.resized)
+          ? (openBlock(), createBlock(Img, {
+              key: 0,
+              preview: data.item?.profileImage?.resized,
+              original: data.item?.profileImage?.original,
+              alt: data.item?.name,
+              skeleton: true
+            }, null, 8, ["preview", "original", "alt"]))
+          : createCommentVNode("", true),
+        createBaseVNode("span", null, toDisplayString(data.item?.name), 1)
+      ])
+    ]),
+    content: withCtx(() => [
+      createBaseVNode("ul", _hoisted_2, [
+        createBaseVNode("li", {
+          class: "clickable",
+          onClick: shareIt
+        }, [
+          createVNode(_component_Icon, { icon: "share" }),
+          createBaseVNode("span", null, toDisplayString(unref(t)('modals.artist-kebab.share')), 1)
+        ]),
+        createBaseVNode("li", {
+          class: "clickable",
+          onClick: about
+        }, [
+          createVNode(_component_Icon, { icon: "help" }),
+          createBaseVNode("span", null, toDisplayString(unref(t)('modals.artist-kebab.about')), 1)
+        ])
+      ])
+    ]),
+    _: 1
+  }, 8, ["isLoading"]))
+}
+}
+
+};
+const ModalArtistKebab = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-862c5667"]]);
+
+export { ModalArtistKebab as default };
+//# sourceMappingURL=ModalArtistKebab-Bfea0Imt.js.map
