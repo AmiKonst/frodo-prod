8e28f9c9ed9bd6153f85cc7dd8c7056aa265830f
AmiKonst <tiufyakin@gmail.com>
2025-11-25 11:29:42 +0300

    Build

---
+++ assets/Error-DrVY0AQO.js
@@ -0,0 +1,27 @@
+import { u as useI18n, r as reactive, b as createBlock, h as unref, o as openBlock } from './index-BnhYjjz0.js';
+import { E as EmptyLabel } from './EmptyLabel-CX4uGAPo.js';
+
+const _sfc_main = {
+  __name: 'Error',
+  setup(__props) {
+
+    const { t, tm, rt } = useI18n();
+
+    const description = tm('pages.error.descriptions').map(item => rt(item));
+
+    const data = reactive({
+        description: description[Math.min(parseInt(Math.random() * description.length), description.length - 1)] 
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(EmptyLabel, {
+    title: unref(t)('pages.error.title'),
+    description: data.description
+  }, null, 8, ["title", "description"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Error-DrVY0AQO.js.map
