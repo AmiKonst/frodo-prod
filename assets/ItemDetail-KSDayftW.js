1213f4f611b46f849553cb61725fd0d6e66e8fb9
AmiKonst <tiufyakin@gmail.com>
2025-11-26 20:13:56 +0300

    build

---
+++ assets/ItemDetail-KSDayftW.js
@@ -0,0 +1,116 @@
+import { _ as _export_sfc, s as stores, r as reactive, i as ref, k as computed, l as onMounted, g as onBeforeUnmount, c as createElementBlock, o as openBlock, v as createBaseVNode, p as renderSlot, n as normalizeStyle } from './index-DOjOZqg-.js';
+
+const _hoisted_1 = { class: "detail-item" };
+const _hoisted_2 = { class: "box" };
+const _hoisted_3 = { class: "toolbar" };
+
+    
+const _sfc_main = {
+  __name: 'ItemDetail',
+  props: {
+        previewHeight: { type: Number, default: 384 },
+    },
+  emits: ['mode'],
+  setup(__props, { emit: __emit }) {
+
+    // import IconButton from '🔗/components/ui/IconButton.vue';
+    stores.others();
+
+    const props = __props;
+
+    const data = reactive({
+        mode: 'preview'
+    });
+
+
+    const emit = __emit;
+
+    const detail = ref(null);
+
+    const scrollTop = ref(0);
+    const scrollDirection = ref(0);
+
+
+    const progress = computed(() =>
+        Math.min(1, scrollTop.value / props.previewHeight)
+    );
+
+    // Opacity
+        const wrapperOpacity = computed(() => 1 - progress.value);
+        const backgroundOpacity = computed(() => 1 - progress.value);
+
+    let lastY = 0;
+    const onScroll = (e) => {
+        const y = e.target.scrollTop;
+        scrollDirection.value = y - lastY;
+        lastY = y;
+
+        scrollTop.value = y;
+        updateMode();
+    };
+
+    const updateMode = () => {
+        const newMode = progress.value >= 1 ? "detail" : "preview";
+        if (newMode !== data.mode) {
+            data.mode = newMode;
+            emit('mode', newMode);
+        }
+    };
+
+
+    document.body.classList.add('with-tg-header');
+    onMounted(() => {
+        document.querySelector('.page').addEventListener('scroll', onScroll);
+    });
+
+    onBeforeUnmount(() => {
+        document.body.classList.remove('with-tg-header');
+        document.querySelector('.page').removeEventListener('scroll', onScroll);
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock("div", _hoisted_1, [
+    createBaseVNode("div", {
+      class: "background",
+      style: normalizeStyle({
+                opacity: backgroundOpacity.value,
+                height: props.previewHeight + 'px'
+            })
+    }, [
+      renderSlot(_ctx.$slots, "background", {}, undefined, true)
+    ], 4),
+    createBaseVNode("div", _hoisted_2, [
+      createBaseVNode("div", {
+        class: "preview",
+        style: normalizeStyle({
+                    opacity: 1,
+                    height: props.previewHeight + 'px'
+                })
+      }, [
+        createBaseVNode("div", {
+          class: "wrapper",
+          style: normalizeStyle({ opacity: wrapperOpacity.value })
+        }, [
+          createBaseVNode("div", _hoisted_3, [
+            renderSlot(_ctx.$slots, "toolbar", {}, undefined, true)
+          ]),
+          renderSlot(_ctx.$slots, "preview", {}, undefined, true)
+        ], 4)
+      ], 4),
+      createBaseVNode("div", {
+        class: "detail",
+        ref_key: "detail",
+        ref: detail
+      }, [
+        renderSlot(_ctx.$slots, "detail", {}, undefined, true)
+      ], 512)
+    ])
+  ]))
+}
+}
+
+};
+const ItemDetail = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-78e074ca"]]);
+
+export { ItemDetail as I };
+//# sourceMappingURL=ItemDetail-KSDayftW.js.map
---
+++ assets/ModalWelcome-Qx4jigiF.js
@@ -0,0 +1,190 @@
+import { _ as _export_sfc, k as computed, c as createElementBlock, o as openBlock, F as Fragment, m as renderList, z as normalizeClass, s as stores, f as storeToRefs, u as useI18n, r as reactive, M as resolveDirective, b as createBlock, t as withCtx, C as withDirectives, v as createBaseVNode, n as normalizeStyle, h as unref, Q as baseUrl, x as toDisplayString, d as createCommentVNode, e as createVNode, L as createTextVNode, B as Button, A as withModifiers, J as BaseModal } from './index-DOjOZqg-.js';
+
+const _sfc_main$1 = {
+  __name: 'Steps',
+  props: {
+        total: { type: Number, default: 1 },
+        value: { type: Number, default: 1 }
+    },
+  setup(__props) {
+
+    const props = __props;
+
+    const items = computed(() => {
+        return Array(props.total);
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock("ul", null, [
+    (openBlock(true), createElementBlock(Fragment, null, renderList(items.value, (item, id) => {
+      return (openBlock(), createElementBlock("li", {
+        class: normalizeClass({ active: id + 1 === props.value })
+      }, null, 2))
+    }), 256))
+  ]))
+}
+}
+
+};
+const Steps = /*#__PURE__*/_export_sfc(_sfc_main$1, [['__scopeId',"data-v-04011cd1"]]);
+
+const _hoisted_1 = ["innerHTML"];
+const _hoisted_2 = { class: "description" };
+const _hoisted_3 = ["innerHTML"];
+const _hoisted_4 = { class: "description" };
+const _hoisted_5 = {
+  key: 1,
+  class: "enter-box"
+};
+
+
+const _sfc_main = {
+  __name: 'ModalWelcome',
+  setup(__props) {
+
+const others = stores.others();
+const tg = stores.tg();
+const { theme } = storeToRefs(others);
+
+const modals = stores.modals();
+
+
+const { t, tm, rt } = useI18n();
+
+const slides = Array.isArray(tm('modals.welcome.slides')) ? tm('modals.welcome.slides').map(item => ({
+    code: rt(item.code),
+    title: rt(item.title),
+    description: rt(item.description)
+})) : [];
+
+const data = reactive({
+    slides: [slides[0]]
+});
+
+const id = computed(() => {
+    return slides.findIndex(item => item.code === data.slides[data.slides.length - 1].code);
+});
+
+const next = () => {
+    let nextId = id.value;
+
+    if (nextId === -1) {
+        return;
+    }
+
+    nextId += 1;
+    if (nextId > slides.length - 1) {
+        nextId = 0;
+    }
+
+    data.slides.push(slides[nextId]);
+};
+
+const prev = () => {
+    let nextId = id.value;
+
+    if (nextId === -1) {
+        return;
+    }
+
+    nextId -= 1;
+    if (nextId < 0) {
+        nextId = slides.length - 1;
+    }
+
+    data.slides.push(slides[nextId]);
+};
+
+const close = () => {
+    localStorage.welcome = true;
+
+    modals.close('welcome');
+};
+
+return (_ctx, _cache) => {
+  const _directive_touch = resolveDirective("touch");
+
+  return (openBlock(), createBlock(BaseModal, {
+    name: "loader",
+    full: true
+  }, {
+    content: withCtx(() => [
+      withDirectives((openBlock(), createElementBlock("div", {
+        class: "welcome-box",
+        onClick: next
+      }, [
+        (openBlock(true), createElementBlock(Fragment, null, renderList(data.slides, (item, itemId) => {
+          return (openBlock(), createElementBlock("div", {
+            key: itemId,
+            style: normalizeStyle({
+                        'background-image': `url(${unref(baseUrl)}/themes/${unref(theme)}/welcome/${item.code}.jpg)`,
+                        'background-color': 'var(--white-500)'
+                    })
+          }, [
+            createBaseVNode("label", {
+              class: "title",
+              innerHTML: item.title
+            }, null, 8, _hoisted_1),
+            createBaseVNode("div", _hoisted_2, toDisplayString(item.description), 1)
+          ], 4))
+        }), 128)),
+        createBaseVNode("div", null, [
+          createBaseVNode("label", {
+            class: "title",
+            innerHTML: data.slides[data.slides.length - 1].title
+          }, null, 8, _hoisted_3),
+          createBaseVNode("div", _hoisted_4, toDisplayString(data.slides[data.slides.length - 1].description), 1),
+          (id.value !== unref(slides).length - 1)
+            ? (openBlock(), createBlock(Steps, {
+                key: 0,
+                total: 3,
+                value: id.value + 1
+              }, null, 8, ["value"]))
+            : createCommentVNode("", true),
+          (id.value === unref(slides).length - 1)
+            ? (openBlock(), createElementBlock("div", _hoisted_5, [
+                createBaseVNode("div", null, [
+                  createBaseVNode("span", null, [
+                    createTextVNode(toDisplayString(unref(t)('modals.welcome.terms')) + " ", 1),
+                    createVNode(Button, {
+                      name: unref(t)('modals.welcome.terms-button'),
+                      class: "tertiary size-s",
+                      onClick: _cache[0] || (_cache[0] = withModifiers($event => (unref(tg).openLink({
+                                        'external_url': 'https://github.com/Bilbo-Music/bilbo-docs-public/blob/main/terms%20of%20service/Bilbo%20Music.md'
+                                    })), ["stop"]))
+                    }, null, 8, ["name"]),
+                    createTextVNode(" " + toDisplayString(unref(t)('modals.welcome.terms-2')), 1)
+                  ])
+                ]),
+                createVNode(Button, {
+                  name: unref(t)('modals.welcome.close'),
+                  onClick: close
+                }, null, 8, ["name"])
+              ]))
+            : createCommentVNode("", true)
+        ])
+      ])), [
+        [
+          _directive_touch,
+          next,
+          "swipe",
+          { left: true }
+        ],
+        [
+          _directive_touch,
+          prev,
+          "swipe",
+          { right: true }
+        ]
+      ])
+    ]),
+    _: 1
+  }))
+}
+}
+
+};
+const ModalWelcome = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-179a1bcb"]]);
+
+export { ModalWelcome as default };
+//# sourceMappingURL=ModalWelcome-Qx4jigiF.js.map
