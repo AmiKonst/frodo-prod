22dfaf02231f837bca768bf08ea9a33a4d60701a
AmiKonst <tiufyakin@gmail.com>
2025-12-11 18:18:16 +0300

    Build

---
+++ assets/Genre-DRjsaVbK.js
@@ -0,0 +1,55 @@
+import { D as Dictionary } from './Dictionary-5_NpW78L.js';
+import { _ as _sfc_main$1 } from './Ready-For89hL4.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-MSeQtd-7.js';
+import './ItemDetail-CMb8UmVT.js';
+import './Tracks-D0Hyu-BI.js';
+import './Block-U3tLf8vb.js';
+import './TitleWithButtons-x6p7dDMd.js';
+import './EmptyLabel-DOzaB_e1.js';
+
+const _sfc_main = {
+  __name: 'Genre',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'genres');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genre-DRjsaVbK.js.map
---
+++ assets/MyArtists-DLup17kD.js
@@ -0,0 +1,61 @@
+import { u as useI18n, s as stores, r as reactive, o as onMounted, a as onBeforeUnmount, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, i as unref, F as Fragment, f as openBlock } from './index-MSeQtd-7.js';
+import { _ as _sfc_main$2 } from './Ready-For89hL4.js';
+import { _ as _sfc_main$1 } from './Artists-B0YyaUqD.js';
+import './EmptyLabel-DOzaB_e1.js';
+import './Block-U3tLf8vb.js';
+import './TitleWithButtons-x6p7dDMd.js';
+
+const _sfc_main = {
+  __name: 'MyArtists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+
+    const tg = stores.tg();
+
+    const data = reactive({
+        ready: false
+    });
+
+    const init = async () => {
+        data.ready = true;
+    };
+
+    onMounted(() => {
+        tg.showBackButton();
+    });
+
+    onBeforeUnmount(() => {
+        tg.hideBackButton();
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(_sfc_main$1, {
+          key: 0,
+          source: {
+            store: 'user',
+            fnName: 'getMyArtists'
+        },
+          handler: "artist",
+          empty: {
+            title: unref(t)('pages.favorite-my-artists.empty.title'),
+            description: unref(t)('pages.favorite-my-artists.empty.description')
+        },
+          title: "pages.favorite-my-artists.title"
+        }, null, 8, ["empty"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$2, {
+      page: "favorite-my-artists",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=MyArtists-DLup17kD.js.map
