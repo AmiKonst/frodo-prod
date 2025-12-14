112a1f8ab919db23e2a91f3407e117bccfb80596
AmiKonst <tiufyakin@gmail.com>
2025-12-14 21:49:41 +0300

    Build

---
+++ assets/Error-BOFTJ42V.js
@@ -0,0 +1,27 @@
+import { u as useI18n, r as reactive, b as createBlock, i as unref, f as openBlock } from './index-C0wleKo-.js';
+import { E as EmptyLabel } from './EmptyLabel-BVxYG6zl.js';
+
+const _sfc_main = {
+  __name: 'Error',
+  setup(__props) {
+
+    const { t, tm, rt } = useI18n();
+
+    const description = tm('pages.error.descriptions').map(item => rt(item));
+
+    const data = reactive({
+        description: description[Math.min(parseInt(Math.random() * description.length), description.length - 1)] 
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(EmptyLabel, {
+    title: unref(t)('pages.error.title'),
+    description: data.description
+  }, null, 8, ["title", "description"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Error-BOFTJ42V.js.map
