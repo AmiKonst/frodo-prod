e86f90411d921e4b211433978a922c6685cad077
AmiKonst <tiufyakin@gmail.com>
2025-12-15 18:50:31 +0300

    Build

---
+++ assets/MyArtists-DaibgUiQ.js
@@ -0,0 +1,61 @@
+import { u as useI18n, s as stores, r as reactive, o as onMounted, a as onBeforeUnmount, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, i as unref, F as Fragment, f as openBlock } from './index-0hYSVCtU.js';
+import { _ as _sfc_main$2 } from './Ready-BweqZ8bi.js';
+import { _ as _sfc_main$1 } from './Artists-DCYEQLFg.js';
+import './EmptyLabel-Dw666zwo.js';
+import './Block-DOf3OjC8.js';
+import './TitleWithButtons-DSSVrfN_.js';
+
+const _sfc_main = {
+  __name: 'MyArtists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+
+    const tg = stores.tg();
+
+    const data = reactive({
+        ready: false
+    });
+
+    const init = async () => {
+        data.ready = true;
+    };
+
+    onMounted(() => {
+        tg.showBackButton();
+    });
+
+    onBeforeUnmount(() => {
+        tg.hideBackButton();
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(_sfc_main$1, {
+          key: 0,
+          source: {
+            store: 'user',
+            fnName: 'getMyArtists'
+        },
+          handler: "artist",
+          empty: {
+            title: unref(t)('pages.favorite-my-artists.empty.title'),
+            description: unref(t)('pages.favorite-my-artists.empty.description')
+        },
+          title: "pages.favorite-my-artists.title"
+        }, null, 8, ["empty"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$2, {
+      page: "favorite-my-artists",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=MyArtists-DaibgUiQ.js.map
