abb948558cd8d632773a9cb2f8a0858622858e66
AmiKonst <tiufyakin@gmail.com>
2025-12-15 18:52:23 +0300

    Build

---
+++ assets/ModalGallery-B-fyHIe2.js
@@ -0,0 +1,202 @@
+import { _ as _export_sfc, s as stores, r as reactive, o as onMounted, a as onBeforeUnmount, P as resolveDirective, b as createBlock, f as openBlock, w as withCtx, y as createBaseVNode, D as withDirectives, c as createElementBlock, B as normalizeStyle, F as Fragment, n as renderList, C as withModifiers, p as normalizeClass, d as createCommentVNode, e as createVNode, Q as Loader, R as Transition, i as unref, E as vShow, L as BaseModal } from './index-BoU5CK3p.js';
+
+const _hoisted_1 = { class: "gallery-wrapper" };
+const _hoisted_2 = ["src", "onLoad"];
+const MIN_SCALE = 1;
+const MAX_SCALE = 4;
+const CLOSE_THRESHOLD = 150;
+const _sfc_main = {
+  __name: "ModalGallery",
+  props: {
+    name: { type: String, default: "gallery" }
+  },
+  setup(__props) {
+    const modals = stores.modals();
+    const props = __props;
+    const BASE_URL = "https://api.wetap-tech-service.ru";
+    const data = reactive({
+      imgs: [],
+      currentIndex: 0,
+      loaded: {},
+      scale: 1,
+      x: 0,
+      y: 0,
+      opacity: 1,
+      rotate: 0,
+      isClosing: false
+    });
+    let start = null;
+    let lastTap = 0;
+    const onZoom = (factor) => {
+      const newScale = Math.max(MIN_SCALE, Math.min(MAX_SCALE, data.scale * factor));
+      const scaleFactor = newScale / data.scale;
+      data.x = data.scale === 1 ? 0 : parseInt(data.x * scaleFactor);
+      data.y = data.scale === 1 ? 0 : parseInt(data.y * scaleFactor);
+      data.scale = newScale;
+    };
+    const onTouchStart = (e) => {
+      if (e.touches.length !== 1) {
+        return;
+      }
+      const now = Date.now();
+      if (now - lastTap < 300) {
+        if (data.scale === 1) {
+          data.scale = MAX_SCALE;
+        } else {
+          resetZoom();
+        }
+        lastTap = 0;
+        e.preventDefault();
+        return;
+      }
+      lastTap = now;
+      start = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+      data.isClosing = false;
+    };
+    const onTouchMove = (e) => {
+      if (!start || e.touches.length !== 1) {
+        return;
+      }
+      const dx = e.touches[0].clientX - start.x;
+      const dy = e.touches[0].clientY - start.y;
+      if (data.scale > 1) {
+        data.x += dx;
+        data.y += dy;
+        data.isClosing = false;
+      } else if (dy) {
+        data.isClosing = true;
+        data.y = Math.max(0, data.y + dy);
+        const factor = Math.min(data.y / (window.innerHeight / 2), 1);
+        data.opacity = 1 - factor * 0.8;
+        data.rotate = factor * 10;
+      }
+      start = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+    };
+    const onTouchEnd = () => {
+      if (data.isClosing) {
+        if (data.y > CLOSE_THRESHOLD) {
+          close();
+        } else {
+          data.y = 0;
+          data.opacity = 1;
+          data.rotate = 0;
+        }
+      }
+      start = null;
+      data.isClosing = false;
+    };
+    const close = () => {
+      modals.close(props.name);
+    };
+    const next = () => {
+      if (data.scale !== 1) {
+        return;
+      }
+      if (data.currentIndex < data.imgs.length - 1) {
+        data.currentIndex += 1;
+        resetZoom();
+        data.loading = true;
+      }
+    };
+    const prev = () => {
+      if (data.scale !== 1) {
+        return;
+      }
+      if (data.currentIndex > 0) {
+        data.currentIndex -= 1;
+        resetZoom();
+      }
+    };
+    const onImageLoad = (id) => {
+      data.loaded[id] = true;
+    };
+    const resetZoom = () => {
+      data.scale = 1;
+      data.x = 0;
+      data.y = 0;
+      data.opacity = 1;
+      data.rotate = 0;
+    };
+    onMounted(() => {
+      const payload = modals.getData(props.name);
+      if (payload?.imgs?.length) {
+        data.imgs = payload.imgs;
+      }
+    });
+    onBeforeUnmount(() => {
+    });
+    return (_ctx, _cache) => {
+      const _directive_touch = resolveDirective("touch");
+      return openBlock(), createBlock(BaseModal, {
+        name: props.name,
+        full: true,
+        mobileMode: true,
+        draggable: false,
+        closeOutside: true
+      }, {
+        content: withCtx(() => [
+          createBaseVNode("div", _hoisted_1, [
+            withDirectives((openBlock(), createElementBlock("div", {
+              class: "slides",
+              style: normalizeStyle({ transform: `translateX(${-data.currentIndex * 100}% )` })
+            }, [
+              (openBlock(true), createElementBlock(Fragment, null, renderList(data.imgs, (img, id) => {
+                return openBlock(), createElementBlock("div", {
+                  class: "slide",
+                  key: id
+                }, [
+                  withDirectives((openBlock(), createElementBlock("div", {
+                    class: normalizeClass(["zoom-container", { loading: !data.loaded[id] }]),
+                    style: normalizeStyle({
+                      transform: `translate(${data.x}px, ${data.y}px) scale(${data.scale}) rotate(${data.rotate}deg)`,
+                      opacity: data.opacity
+                    }),
+                    onTouchstart: onTouchStart,
+                    onTouchmove: withModifiers(onTouchMove, ["prevent"]),
+                    onTouchend: onTouchEnd
+                  }, [
+                    !data.loaded[id] ? (openBlock(), createBlock(Loader, {
+                      key: 0,
+                      class: "opacity-mode"
+                    })) : createCommentVNode("", true),
+                    createVNode(Transition, { name: "fade" }, {
+                      default: withCtx(() => [
+                        withDirectives(createBaseVNode("img", {
+                          src: `${unref(BASE_URL)}${img.original || img.resized}`,
+                          onLoad: ($event) => onImageLoad(id)
+                        }, null, 40, _hoisted_2), [
+                          [vShow, data.loaded[id]]
+                        ])
+                      ]),
+                      _: 2
+                    }, 1024)
+                  ], 38)), [
+                    [_directive_touch, onZoom, "zoom"]
+                  ])
+                ]);
+              }), 128))
+            ], 4)), [
+              [
+                _directive_touch,
+                next,
+                "swipe",
+                { left: true }
+              ],
+              [
+                _directive_touch,
+                prev,
+                "swipe",
+                { right: true }
+              ]
+            ])
+          ])
+        ]),
+        _: 1
+      }, 8, ["name"]);
+    };
+  }
+};
+const ModalGallery = /* @__PURE__ */ _export_sfc(_sfc_main, [["__scopeId", "data-v-cb891d49"]]);
+
+export { ModalGallery as default };
+//# sourceMappingURL=ModalGallery-B-fyHIe2.js.map
---
+++ assets/Mood-ci5RffCk.js
@@ -0,0 +1,55 @@
+import { D as Dictionary } from './Dictionary-BeLSK0Sy.js';
+import { _ as _sfc_main$1 } from './Ready-jlC_vKVM.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-BoU5CK3p.js';
+import './ItemDetail-B3KJTsNn.js';
+import './Tracks-D7fifSWG.js';
+import './Block-BZ5lJclG.js';
+import './TitleWithButtons-BncmSdAa.js';
+import './EmptyLabel-CCTJ6IxW.js';
+
+const _sfc_main = {
+  __name: 'Mood',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'moods');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Mood-ci5RffCk.js.map
