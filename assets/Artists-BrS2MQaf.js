f24077b6bf4a3276592d398e10091e2f0f2019d3
AmiKonst <tiufyakin@gmail.com>
2025-12-15 18:24:47 +0300

    Build

---
+++ assets/Artists-BrS2MQaf.js
@@ -0,0 +1,65 @@
+import { u as useI18n, s as stores, r as reactive, o as onMounted, a as onBeforeUnmount, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-CfLbgll1.js';
+import { _ as _sfc_main$2 } from './Ready-Czi5SsJa.js';
+import { _ as _sfc_main$1 } from './Artists-Dix7Ipz5.js';
+import './Block-FDF27ShT.js';
+import './TitleWithButtons-B1iJwcyt.js';
+import './EmptyLabel-D_MtDEe_.js';
+
+const _sfc_main = {
+  __name: 'Artists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    const tg = stores.tg();
+
+    const data = reactive({
+        ready: false,
+        item: null
+    });
+
+    const init = async () => {
+        data.ready = true;
+    };
+
+
+    onMounted(async () => {
+        tg.showBackButton();
+    });
+
+    onBeforeUnmount(() => {
+        tg.hideBackButton();
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(_sfc_main$1, {
+          key: 0,
+          take: 30,
+          skeletonCount: 12,
+          params: {
+            state: 'PUBLISHED'
+        },
+          sort: {
+            code: 'createdAt',
+            direction: 'desc'
+        },
+          showKebab: true,
+          loadMore: true,
+          handler: "artist",
+          title: "blocks.artists.new-artists"
+        }))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$2, {
+      page: `artists-list`,
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Artists-BrS2MQaf.js.map
---
+++ assets/Mood-CGpww9p5.js
@@ -0,0 +1,55 @@
+import { D as Dictionary } from './Dictionary-B_Hmf6kb.js';
+import { _ as _sfc_main$1 } from './Ready-Czi5SsJa.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-CfLbgll1.js';
+import './ItemDetail-DkSs4Hv9.js';
+import './Tracks-B_vDxzpu.js';
+import './Block-FDF27ShT.js';
+import './TitleWithButtons-B1iJwcyt.js';
+import './EmptyLabel-D_MtDEe_.js';
+
+const _sfc_main = {
+  __name: 'Mood',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'moods');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Mood-CGpww9p5.js.map
