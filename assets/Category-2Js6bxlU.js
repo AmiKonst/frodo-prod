1c1b66b57c535c377be0116c12f58ddf484cbec0
AmiKonst <tiufyakin@gmail.com>
2025-12-16 14:45:12 +0300

    Build

---
+++ assets/Category-2Js6bxlU.js
@@ -0,0 +1,55 @@
+import { D as Dictionary } from './Dictionary-zl-1vsBP.js';
+import { _ as _sfc_main$1 } from './Ready-DbsQXPVB.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-DSSyEy4p.js';
+import './ItemDetail-V-cY5A3C.js';
+import './Tracks-BjFObRq2.js';
+import './Block-CSuxk3mP.js';
+import './TitleWithButtons-oU9zXYRb.js';
+import './EmptyLabel-Cbl-nrLS.js';
+
+const _sfc_main = {
+  __name: 'Category',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'categories');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "category",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Category-2Js6bxlU.js.map
