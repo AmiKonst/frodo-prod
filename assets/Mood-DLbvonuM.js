ee4e3757a13e14a44579a62d3b64daeb4ead7a71
AmiKonst <tiufyakin@gmail.com>
2026-01-13 12:13:09 +0300

    Build

---
+++ assets/Mood-DLbvonuM.js
@@ -0,0 +1,55 @@
+import { D as Dictionary } from './Dictionary-D_GXebWg.js';
+import { _ as _sfc_main$1 } from './Ready-lemX0vo9.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-CtNKyRc2.js';
+import './ItemDetail-Ck0kagWM.js';
+import './Tracks-6Djb9wfi.js';
+import './Block-DF4lYS_M.js';
+import './TitleWithButtons-BVyYTYY_.js';
+import './EmptyLabel-CEAU7FJW.js';
+
+const _sfc_main = {
+  __name: 'Mood',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'moods');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Mood-DLbvonuM.js.map
