950aa675d0033e0e5597bd02957dfdbe4ae333a5
AmiKonst <tiufyakin@gmail.com>
2026-01-14 13:23:04 +0300

    Build

---
+++ assets/Genre-BNaOGmEp.js
@@ -0,0 +1,55 @@
+import { D as Dictionary } from './Dictionary-DkLb32Ue.js';
+import { _ as _sfc_main$1 } from './Ready-0i5AwKQy.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-TltiKT0-.js';
+import './ItemDetail-toayQpEi.js';
+import './Tracks-D2-oM_Wo.js';
+import './Block-DOkNO46Y.js';
+import './TitleWithButtons-Z37rBoWm.js';
+import './EmptyLabel-D7cauNwD.js';
+
+const _sfc_main = {
+  __name: 'Genre',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'genres');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genre-BNaOGmEp.js.map
