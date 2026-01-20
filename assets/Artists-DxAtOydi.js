950eb258cfcd88e3f0afe7109c32f1e196a5c875
AmiKonst <tiufyakin@gmail.com>
2026-01-20 12:42:30 +0300

    Build

---
+++ assets/Artists-DxAtOydi.js
@@ -0,0 +1,51 @@
+import { _ as _export_sfc, u as useI18n, s as stores, o as onMounted, a as onBeforeUnmount, c as createElementBlock, n as createBaseVNode, e as createVNode, y as toDisplayString, i as unref, F as Fragment, f as openBlock } from './index-D3rr0FWO.js';
+import { _ as _sfc_main$1 } from './Ready-Dru0EF4_.js';
+import { E as EmptyLabel } from './EmptyLabel-Dxiw-Yf7.js';
+
+const _hoisted_1 = { class: "head" };
+const _hoisted_2 = { class: "title" };
+
+    
+const _sfc_main = {
+  __name: 'Artists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    // import { storeToRefs } from 'pinia';
+    stores.user();
+    stores.nav();
+    const tg = stores.tg();
+
+    stores.others();
+    // const { ready } = storeToRefs(others);
+
+    onMounted(() => {
+        tg.showBackButton();
+    });
+
+    onBeforeUnmount(() => {
+        tg.hideBackButton();
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    createBaseVNode("div", _hoisted_1, [
+      createBaseVNode("label", _hoisted_2, toDisplayString(unref(t)('pages.favorite-artists.title')), 1)
+    ]),
+    createVNode(EmptyLabel, {
+      class: "empty-label",
+      "animate-icon": "artists",
+      title: unref(t)('pages.favorite-artists.empty.title'),
+      description: unref(t)('pages.favorite-artists.empty.description')
+    }, null, 8, ["title", "description"]),
+    createVNode(_sfc_main$1, { page: "favorite-artists" })
+  ], 64))
+}
+}
+
+};
+const Artists = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-7e149a1b"]]);
+
+export { Artists as default };
+//# sourceMappingURL=Artists-DxAtOydi.js.map
---
+++ assets/Error-D2ZaAcLa.js
@@ -0,0 +1,27 @@
+import { u as useI18n, r as reactive, b as createBlock, i as unref, f as openBlock } from './index-D3rr0FWO.js';
+import { E as EmptyLabel } from './EmptyLabel-Dxiw-Yf7.js';
+
+const _sfc_main = {
+  __name: 'Error',
+  setup(__props) {
+
+    const { t, tm, rt } = useI18n();
+
+    const description = tm('pages.error.descriptions').map(item => rt(item));
+
+    const data = reactive({
+        description: description[Math.min(parseInt(Math.random() * description.length), description.length - 1)] 
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(EmptyLabel, {
+    title: unref(t)('pages.error.title'),
+    description: data.description
+  }, null, 8, ["title", "description"]))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Error-D2ZaAcLa.js.map
