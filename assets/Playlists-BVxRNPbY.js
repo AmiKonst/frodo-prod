be63b40fcdfbfe7ffef89a81f193e8bb7558bdfd
AmiKonst <tiufyakin@gmail.com>
2026-01-30 18:18:55 +0300

    Build

---
+++ assets/Playlists-BVxRNPbY.js
@@ -0,0 +1,48 @@
+import { _ as _export_sfc, u as useI18n, s as stores, o as onMounted, a as onBeforeUnmount, c as createElementBlock, n as createBaseVNode, e as createVNode, y as toDisplayString, i as unref, F as Fragment, f as openBlock } from './index-BuiHZ4M4.js';
+import { _ as _sfc_main$1 } from './Ready-B112lb7Y.js';
+import { E as EmptyLabel } from './EmptyLabel-D_Gjejf8.js';
+
+const _hoisted_1 = { class: "head" };
+const _hoisted_2 = { class: "title" };
+
+    
+const _sfc_main = {
+  __name: 'Playlists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    // import { storeToRefs } from 'pinia';
+    stores.user();
+    stores.nav();
+    stores.tg();
+
+    stores.others();
+
+    onMounted(() => {
+    });
+
+    onBeforeUnmount(() => {
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    createBaseVNode("div", _hoisted_1, [
+      createBaseVNode("label", _hoisted_2, toDisplayString(unref(t)('pages.favorite-playlists.title')), 1)
+    ]),
+    createVNode(EmptyLabel, {
+      class: "empty-label",
+      "animate-icon": "playlists",
+      title: unref(t)('pages.favorite-playlists.empty.title'),
+      description: unref(t)('pages.favorite-playlists.empty.description')
+    }, null, 8, ["title", "description"]),
+    createVNode(_sfc_main$1, { page: "favorite-playlists" })
+  ], 64))
+}
+}
+
+};
+const Playlists = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-2cae6bb1"]]);
+
+export { Playlists as default };
+//# sourceMappingURL=Playlists-BVxRNPbY.js.map
