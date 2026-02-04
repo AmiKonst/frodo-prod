d32d5e9d9245de1ba85b3de1980503494ec8bc6d
AmiKonst <tiufyakin@gmail.com>
2026-02-04 21:40:02 +0300

    Build

---
+++ assets/Artists-M9BJqcOh.js
@@ -0,0 +1,63 @@
+import { u as useI18n, s as stores, r as reactive, o as onMounted, a as onBeforeUnmount, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-CLhYCOeI.js';
+import { _ as _sfc_main$2 } from './Ready-CMrdBbFj.js';
+import { _ as _sfc_main$1 } from './Artists-CwQXeVk0.js';
+import './Block-DI4DfS1C.js';
+import './TitleWithButtons-C_eJve0U.js';
+import './EmptyLabel-BphJdalu.js';
+
+const _sfc_main = {
+  __name: 'Artists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    stores.tg();
+
+    const data = reactive({
+        ready: false,
+        item: null
+    });
+
+    const init = async () => {
+        data.ready = true;
+    };
+
+
+    onMounted(async () => {
+    });
+
+    onBeforeUnmount(() => {
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(_sfc_main$1, {
+          key: 0,
+          take: 30,
+          skeletonCount: 12,
+          params: {
+            state: 'PUBLISHED'
+        },
+          sort: {
+            code: 'createdAt',
+            direction: 'desc'
+        },
+          showKebab: true,
+          loadMore: true,
+          handler: "artist",
+          title: "blocks.artists.new-artists"
+        }))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$2, {
+      page: `artists-list`,
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Artists-M9BJqcOh.js.map
