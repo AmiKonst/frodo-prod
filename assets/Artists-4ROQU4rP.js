b5323f6a72cdaa89c0ee06d8afd0be9cfbbedbd1
AmiKonst <tiufyakin@gmail.com>
2026-02-06 19:35:07 +0300

    Build

---
+++ assets/Artists-4ROQU4rP.js
@@ -0,0 +1,49 @@
+import { _ as _export_sfc, u as useI18n, s as stores, o as onMounted, a as onBeforeUnmount, c as createElementBlock, n as createBaseVNode, e as createVNode, y as toDisplayString, i as unref, F as Fragment, f as openBlock } from './index-DCNJa9Wi.js';
+import { _ as _sfc_main$1 } from './Ready-DvXVXCcW.js';
+import { E as EmptyLabel } from './EmptyLabel-BWkra0Re.js';
+
+const _hoisted_1 = { class: "head" };
+const _hoisted_2 = { class: "title" };
+
+    
+const _sfc_main = {
+  __name: 'Artists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    // import { storeToRefs } from 'pinia';
+    stores.user();
+    stores.nav();
+    stores.tg();
+
+    stores.others();
+    // const { ready } = storeToRefs(others);
+
+    onMounted(() => {
+    });
+
+    onBeforeUnmount(() => {
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    createBaseVNode("div", _hoisted_1, [
+      createBaseVNode("label", _hoisted_2, toDisplayString(unref(t)('pages.favorite-artists.title')), 1)
+    ]),
+    createVNode(EmptyLabel, {
+      class: "empty-label",
+      "animate-icon": "artists",
+      title: unref(t)('pages.favorite-artists.empty.title'),
+      description: unref(t)('pages.favorite-artists.empty.description')
+    }, null, 8, ["title", "description"]),
+    createVNode(_sfc_main$1, { page: "favorite-artists" })
+  ], 64))
+}
+}
+
+};
+const Artists = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-60b0e87c"]]);
+
+export { Artists as default };
+//# sourceMappingURL=Artists-4ROQU4rP.js.map
