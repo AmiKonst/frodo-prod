424287f6cd0db0bac7eed8d4c5303818862bd935
AmiKonst <tiufyakin@gmail.com>
2026-02-09 23:09:32 +0300

    Build

---
+++ assets/Genre-B107QIfQ.js
@@ -0,0 +1,56 @@
+import { D as Dictionary } from './Dictionary-TtbkF6TF.js';
+import { _ as _sfc_main$1 } from './Ready-BoHRqfzw.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-BWKG_aXv.js';
+import './ItemDetail-2TX13ET5.js';
+import './Tracks-CriGMNqg.js';
+import './Block-ABqmGA1C.js';
+import './TitleWithButtons-BEEG00E0.js';
+import './EmptyLabel-9HpF0ouj.js';
+import './Collage-CiOpoLAg.js';
+
+const _sfc_main = {
+  __name: 'Genre',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'genres');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Genre-B107QIfQ.js.map
---
+++ assets/TitleWithButtons-BEEG00E0.js
@@ -0,0 +1,57 @@
+import { _ as _export_sfc, s as stores, u as useI18n, c as createElementBlock, f as openBlock, b as createBlock, d as createCommentVNode, O as mergeProps, p as Img, v as toDisplayString, h as unref, F as Fragment, m as renderList, B as Button } from './index-BWKG_aXv.js';
+
+const _hoisted_1 = { class: "title" };
+const _hoisted_2 = {
+  key: 1,
+  class: "title-text"
+};
+
+    
+const _sfc_main = {
+  __name: 'TitleWithButtons',
+  props: {
+        title: { type: [Object, String, null], default: null },
+        items: { type: Array, default: () => [] }
+    },
+  setup(__props) {
+
+    stores.locale();
+    const nav = stores.nav();
+
+    const { t } = useI18n();
+
+    const props = __props;
+
+    const onClick = (item) => {
+        if (item.page) {
+            nav.open(item.page, item.params);
+        }
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock("label", _hoisted_1, [
+    (props.title?.img)
+      ? (openBlock(), createBlock(Img, mergeProps({ key: 0 }, props.title.img, {
+          style: { width: props.title.img.width }
+        }), null, 16, ["style"]))
+      : createCommentVNode("", true),
+    (props.title)
+      ? (openBlock(), createElementBlock("span", _hoisted_2, toDisplayString(props.title?.text || unref(t)(props.title)), 1))
+      : createCommentVNode("", true),
+    (openBlock(true), createElementBlock(Fragment, null, renderList(props.items, (item, id) => {
+      return (openBlock(), createBlock(Button, {
+        key: item.id,
+        name: unref(t)(item.name),
+        class: "size-s secondary",
+        onClick: $event => (onClick(item))
+      }, null, 8, ["name", "onClick"]))
+    }), 128))
+  ]))
+}
+}
+
+};
+const TitleWithButtons = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-f4f18980"]]);
+
+export { TitleWithButtons as T };
+//# sourceMappingURL=TitleWithButtons-BEEG00E0.js.map
