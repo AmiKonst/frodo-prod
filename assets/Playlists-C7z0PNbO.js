d2c7fc73c148f59b88a6d29ecd2be1cd8b2a19f2
AmiKonst <tiufyakin@gmail.com>
2026-02-12 19:22:50 +0300

    Build

---
+++ assets/Playlists-C7z0PNbO.js
@@ -0,0 +1,48 @@
+import { _ as _export_sfc, u as useI18n, s as stores, o as onMounted, a as onBeforeUnmount, c as createElementBlock, l as createBaseVNode, e as createVNode, v as toDisplayString, h as unref, F as Fragment, f as openBlock } from './index-gV8FgsDe.js';
+import { _ as _sfc_main$1 } from './Ready-CUpFw5wo.js';
+import { E as EmptyLabel } from './EmptyLabel-ClZUB55N.js';
+
+const _hoisted_1 = { class: "head" };
+const _hoisted_2 = { class: "title" };
+
+    
+const _sfc_main = {
+  __name: 'Playlists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    // import { storeToRefs } from 'pinia';
+    stores.user();
+    stores.nav();
+    stores.tg();
+
+    stores.others();
+
+    onMounted(() => {
+    });
+
+    onBeforeUnmount(() => {
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    createBaseVNode("div", _hoisted_1, [
+      createBaseVNode("label", _hoisted_2, toDisplayString(unref(t)('pages.favorite-playlists.title')), 1)
+    ]),
+    createVNode(EmptyLabel, {
+      class: "empty-label",
+      "animate-icon": "playlists",
+      title: unref(t)('pages.favorite-playlists.empty.title'),
+      description: unref(t)('pages.favorite-playlists.empty.description')
+    }, null, 8, ["title", "description"]),
+    createVNode(_sfc_main$1, { page: "favorite-playlists" })
+  ], 64))
+}
+}
+
+};
+const Playlists = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-2cae6bb1"]]);
+
+export { Playlists as default };
+//# sourceMappingURL=Playlists-C7z0PNbO.js.map
---
+++ assets/Tracks-Dd91IVYk.js
@@ -0,0 +1,78 @@
+import { u as useI18n, s as stores, r as reactive, o as onMounted, a as onBeforeUnmount, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock, g as api } from './index-gV8FgsDe.js';
+import { _ as _sfc_main$2 } from './Ready-CUpFw5wo.js';
+import { _ as _sfc_main$1 } from './Tracks-Db3ouGc3.js';
+import './Block-DIvdLCcV.js';
+import './TitleWithButtons-CthE1Ogy.js';
+import './EmptyLabel-ClZUB55N.js';
+import './Collage-BYtfvRDz.js';
+
+const _sfc_main = {
+  __name: 'Tracks',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const { t } = useI18n();
+
+    const tg = stores.tg();
+    const modals = stores.modals();
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        item: null
+    });
+
+    const init = async () => {
+        data.item = await api.artists().getByCode(props.code);
+        data.ready = true;
+    };
+
+    const openArtistKebab = () => {
+        modals.open('artist-kebab', { quietClose: true, data: { code: props.code } });
+    };
+
+    onMounted(async () => {
+        tg.showSettingsButton(openArtistKebab);
+    });
+
+    onBeforeUnmount(() => {
+        tg.hideSettingsButton(openArtistKebab);
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.item?.id)
+      ? (openBlock(), createBlock(_sfc_main$1, {
+          key: 0,
+          params: {
+            contributors: data.item.id,
+            state: 'PUBLISHED'
+        },
+          sort: {
+            code: 'ratingTotal',
+            direction: 'desc'
+        },
+          options: {
+            type: 'artist',
+            id: data.item.id
+        },
+          showKebab: true,
+          loadMore: true,
+          title: "blocks.tracks.popular-tracks"
+        }, null, 8, ["params", "options"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$2, {
+      page: `artist-tracks-${props.code}`,
+      init: init
+    }, null, 8, ["page"])
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Tracks-Dd91IVYk.js.map
