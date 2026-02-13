bd7bb0ede2d59a95ee9e7187e12a4d8b226f7ca4
AmiKonst <tiufyakin@gmail.com>
2026-02-13 15:04:14 +0300

    Build

---
+++ assets/ModalStudioAuth-DAiP3azZ.js
@@ -0,0 +1,183 @@
+import { _ as _export_sfc, r as reactive, x as ref, w as watch, o as onMounted, c as createElementBlock, f as openBlock, F as Fragment, m as renderList, s as stores, u as useI18n, b as createBlock, k as withCtx, l as createBaseVNode, e as createVNode, h as unref, B as Button, P as createTextVNode, v as toDisplayString, K as BaseModal, q as message } from './index-9wade2Q3.js';
+
+const _hoisted_1$1 = { class: "code-inputs" };
+const _hoisted_2 = ["onInput", "onFocus", "onKeydown"];
+    
+const _sfc_main$1 = {
+  __name: 'Code',
+  props: {
+        count: { type: Number, default: 6 }
+    },
+  emits: ['change'],
+  setup(__props, { emit: __emit }) {
+
+    const props = __props;
+
+    const data = reactive({
+        values: ref(Array(props.count).fill('')),
+        inputs: {}
+    });
+
+    const emit = __emit;
+
+    const onInput = (e, id) => {
+        if (/^[0-9]$/.test(e.target.value)) {
+            data.values[id] = +e.target.value;
+            nextInput(id);
+        } else {
+            data.values[id] = '';
+        }
+    };
+
+    const focusInput = (id) => {
+        if (data.inputs[id]) {
+            data.inputs[id].focus();
+        }
+    };
+
+    const nextInput = (id) => {
+        if (id + 1 < props.count) {
+            focusInput(id + 1);
+        }
+    };
+
+    const onKeydown = (id, event) => {
+        if (event.key === 'Backspace' && !data.values[id] && id > 0) {
+            data.values[id - 1] = '';
+            focusInput(id - 1);
+        }
+    };
+
+    watch(data.values, (newValues) => {
+        if (data.values.every(item => /^[0-9]$/.test(item))) {
+            emit('change', data.values.join(''));
+        } else {
+            emit('change', null);
+        }
+    });
+
+    onMounted(() => {
+        focusInput(0);
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock("div", _hoisted_1$1, [
+    (openBlock(true), createElementBlock(Fragment, null, renderList(data.values, (digit, id) => {
+      return (openBlock(), createElementBlock("input", {
+        key: id,
+        ref_for: true,
+        ref: el => data.inputs[id] = el,
+        placeholder: " ",
+        type: "text",
+        maxlength: "1",
+        onInput: $event => (onInput($event, id)),
+        onFocus: $event => (focusInput(id)),
+        onKeydown: $event => (onKeydown(id, $event))
+      }, null, 40, _hoisted_2))
+    }), 128))
+  ]))
+}
+}
+
+};
+const Code = /*#__PURE__*/_export_sfc(_sfc_main$1, [['__scopeId',"data-v-c41197db"]]);
+
+const _hoisted_1 = ["innerHTML"];
+
+const _sfc_main = {
+  __name: 'ModalStudioAuth',
+  props: {
+    name: {
+        type: String,
+        default: 'sudio-auth'
+    }
+},
+  setup(__props) {
+
+const modals = stores.modals();
+const user = stores.user();
+
+const { t } = useI18n();
+
+const data = reactive({
+    once: false,
+    loading: false,
+    code: ''
+});
+
+const props = __props;
+
+const setCode = (code) => {
+    data.code = code;
+
+    if (data.code && !data.once) {
+        data.once = true;
+        verify();
+    }
+};
+
+const verify = async () => {
+    data.loading = true;
+
+    const payload = await user.externalLogin(data.code);
+
+    if (!payload) {
+        setTimeout(() => {
+            message.info(t('modals.studio-auth.fail.title'));
+            data.loading = false;
+        }, 500);
+        return;
+    }
+
+    setTimeout(() => {
+        modals.open('success', {
+            data: {
+                title: t('modals.studio-auth.done.title'),
+                description: t('modals.studio-auth.done.description'),
+                close: t('modals.studio-auth.done.close')
+            },
+            quietClose: true
+        });
+
+        modals.close(props.name);
+    }, 1000);
+};
+
+onMounted(async () => {
+    data.loading = false;
+});
+
+return (_ctx, _cache) => {
+  return (openBlock(), createBlock(BaseModal, {
+    isLoading: data.loading,
+    name: props.name,
+    mobileMode: true,
+    closeOutside: true,
+    class: "modal-studio-auth"
+  }, {
+    title: withCtx(() => [
+      createTextVNode(toDisplayString(unref(t)('modals.studio-auth.title')), 1)
+    ]),
+    content: withCtx(() => [
+      createBaseVNode("span", {
+        class: "description",
+        innerHTML: unref(t)('modals.studio-auth.description')
+      }, null, 8, _hoisted_1),
+      createVNode(Code, { onChange: setCode }),
+      createVNode(Button, {
+        onClick: verify,
+        disabled: !data.code,
+        class: "size-l",
+        name: unref(t)('modals.studio-auth.verify')
+      }, null, 8, ["disabled", "name"])
+    ]),
+    _: 1
+  }, 8, ["isLoading", "name"]))
+}
+}
+
+};
+const ModalStudioAuth = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-133f6701"]]);
+
+export { ModalStudioAuth as default };
+//# sourceMappingURL=ModalStudioAuth-DAiP3azZ.js.map
---
+++ assets/Mood-DBXmICPi.js
@@ -0,0 +1,56 @@
+import { D as Dictionary } from './Dictionary-BrFrvCIn.js';
+import { _ as _sfc_main$1 } from './Ready-Q36T0jwO.js';
+import { g as api, s as stores, r as reactive, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, F as Fragment, f as openBlock } from './index-9wade2Q3.js';
+import './ItemDetail-B8qY_yUE.js';
+import './Tracks-B23XjofD.js';
+import './Block-BryIZJET.js';
+import './TitleWithButtons-DIXQ32OF.js';
+import './EmptyLabel-KwcMJN5p.js';
+import './Collage-CilAfKFU.js';
+
+const _sfc_main = {
+  __name: 'Mood',
+  props: {
+        code: { type: String }
+    },
+  setup(__props) {
+
+    const dict = api.dict();
+    stores.locale();
+
+
+    const props = __props;
+
+    const data = reactive({
+        ready: false,
+        type: null
+    });
+
+    const init = async () => {
+        const types = await dict.getTypes();
+
+        data.type = types.find(item => item.code === 'moods');
+        data.ready = true;
+    };
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(Dictionary, {
+          key: 0,
+          code: props.code,
+          type: data.type
+        }, null, 8, ["code", "type"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$1, {
+      page: "mood",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+
+export { _sfc_main as default };
+//# sourceMappingURL=Mood-DBXmICPi.js.map
