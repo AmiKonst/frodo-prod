76089f2453e3a210820f95b2d5f0e5077bd78cb9
AmiKonst <tiufyakin@gmail.com>
2026-04-21 18:35:05 +0300

    Build

---
+++ assets/Playlists-BfEEjgdX.js
@@ -0,0 +1,67 @@
+import { _ as _export_sfc, u as useI18n, r as reactive, o as onMounted, a as onBeforeUnmount, c as createElementBlock, b as createBlock, d as createCommentVNode, e as createVNode, h as unref, F as Fragment, f as openBlock } from './index-BzMg08Mj.js';
+import { _ as _sfc_main$2 } from './Ready-NDFoU1p5.js';
+import { _ as _sfc_main$1 } from './Playlists-CiYRzbvJ.js';
+import './Block-DnM3pBfR.js';
+import './TitleWithButtons-wwZjJxCU.js';
+import './EmptyLabel-U_rEx4-z.js';
+/* empty css                                                                         */
+
+// import EmptyLabel from '🔗/components/ui/EmptyLabel.vue';
+    // import stores from '@/stores';
+
+    
+const _sfc_main = {
+  __name: 'Playlists',
+  setup(__props) {
+
+    const { t } = useI18n();
+
+
+    // const messengers = stores.messengers();
+
+    const data = reactive({
+        ready: false
+    });
+
+    const init = async () => {
+        data.ready = true;
+    };
+
+    onMounted(() => {
+    });
+
+    onBeforeUnmount(() => {
+    });
+
+return (_ctx, _cache) => {
+  return (openBlock(), createElementBlock(Fragment, null, [
+    (data.ready)
+      ? (openBlock(), createBlock(_sfc_main$1, {
+          key: 0,
+          source: {
+            store: 'favorite',
+            fnName: 'getPlaylists'
+        },
+          handler: "playlist",
+          empty: {
+            icon: 'playlist',
+            title: unref(t)('pages.favorite.playlists.empty.title'),
+            description: unref(t)('pages.favorite.playlists.empty.description')
+        },
+          showKebab: true,
+          title: "pages.favorite.playlists.title"
+        }, null, 8, ["empty"]))
+      : createCommentVNode("", true),
+    createVNode(_sfc_main$2, {
+      page: "favorite-playlists",
+      init: init
+    })
+  ], 64))
+}
+}
+
+};
+const Playlists = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-a6286137"]]);
+
+export { Playlists as default };
+//# sourceMappingURL=Playlists-BfEEjgdX.js.map
